ece7c425880c02d2dd8d4b0959b9902d553e7ed2
William L. Anderson <band@acm.org>
2021-11-04 11:48:35 -0500

    squeeze multiple spaces; adjust grep pattern

--- zshbench/co2trends.sh
+++ zshbench/co2trends.sh
@@ -1,29 +1,29 @@
 co2trends ()
 {
-    : retrieve latest, 1 year ago, and 10 year ago average CO2 reading from NOAA
-    : uses: awk, basename, curl, grep, tail
+    : retrieve latest, 1 year ago, and 10 year ago average CO2 trend reading from NOAA
+    : uses: awk, basename, curl, grep, tail, tr
     : run:  source co2trends.sh   
     : note: a change to echo messages onto STDERR.
     : to see the "canonical format", declare -f co2trends
     set -- ftp://aftp.cmdl.noaa.gov/products/trends/co2/co2_trend_gl.txt
     df=$(basename $1)
-    curl -q $1 > $df || { echo curl error                    1>&2; return 1; }
+    curl -q $1 | tr -s ' ' > $df || { echo curl error                    1>&2; return 1; }
     :
     set -- $(tail -n 1 $df) && yr=$1 && mo=$2 && dy=$3
     :
-    grep "$yr    $mo    $dy" $df | awk '
-    BEGIN { fmt = "Latest (%s-%02d-%02d) average global co2 value (NOAA): %s ppm\n" } 
-          {  printf( fmt, $1, $2, $3, $4 )
+    grep "$yr $mo $dy " $df | awk '
+    BEGIN { fmt = "Latest (%s-%02d-%02d) average global co2 trend value (NOAA): %s ppm\n" } 
+          {  printf( fmt, $1, $2, $3, $5 )
           }
 '
-    grep "$(($yr-1))    $mo    $dy" $df | awk '
-    BEGIN { fmt = "One year ago (%s-%02d-%02d) average global co2 value (NOAA): %s ppm\n" } 
-          {  printf( fmt, $1, $2, $3, $4 )
+    grep "$(($yr-1)) $mo $dy " $df | awk '
+    BEGIN { fmt = "One year ago (%s-%02d-%02d) average global co2 trend value (NOAA): %s ppm\n" } 
+          {  printf( fmt, $1, $2, $3, $5 )
           }
 '
-    grep "$(($yr-10))    $mo    $dy" $df | awk '
-    BEGIN { fmt = "Ten years ago (%s-%02d-%02d) average global co2 value (NOAA): %s ppm\n" } 
-          {  printf( fmt, $1, $2, $3, $4 )
+    grep "$(($yr-10)) $mo $dy " $df | awk '
+    BEGIN { fmt = "Ten years ago (%s-%02d-%02d) average global co2 trend value (NOAA): %s ppm\n" } 
+          {  printf( fmt, $1, $2, $3, $5 )
           }
 '
     rm $df
