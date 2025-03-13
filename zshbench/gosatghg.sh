0b0aeeb9043c05d8a17f9b1b673377930a7e6a06
William L. Anderson <band@users.noreply.github.com>
2025-03-13 11:25:17 -0500

    catch curl error; scrape the webpage

--- zshbench/gosatghg.sh
+++ zshbench/gosatghg.sh
@@ -1,11 +1,16 @@
 gosatghg ()
 {
     : retrieve latest monthly mean CO2 and CH4 readings from Japan NIES GOSAT project data
-    : uses: awk, basename, curl, echo, tail, tr, unzip
+    : uses: awk, basename, curl, echo, egrep, head, sed, tail, tr, unzip
     : run:  source gosatghg.sh
     : to see the "canonical format", which gosatghg
     : co2 data first
-    set -- https://www.gosat.nies.go.jp/assets/whole-atmosphere-monthly-mean_co2_$(date -v-2m "+%B_%Y" | tr '[:upper:]' '[:lower:]').txt.zip
+    if ! output=$(curl -q https://www.gosat.nies.go.jp/en/recent-global-co2.html > rg-co2.html); then
+	echo "$output" >$2
+	return $?
+    fi
+    data_link=$(egrep -o 'href="\.\./assets/[^"]+"' rg-co2.html | head -1 | sed 's|href="\.\./\(.*\)"|\1|g')
+    set -- https://www.gosat.nies.go.jp/$data_link
     local wamm_zip="$(basename $1)"
     if ! output=$(curl --connect-timeout 5 -q $1 > $wamm_zip); then
         echo "$output" >&2
@@ -18,8 +23,14 @@ gosatghg ()
     	 { fmt = "Latest %s-%02d monthly CO2 mean value: %s (ppm) | trend value: %s (ppm)\n" }
          { printf( fmt, $1, $2, $3, $4 ) }
     '
+    rm rg-co2.html $wamm_zip $wamm_text
     : ch4 data next
-    set -- https://www.gosat.nies.go.jp/assets/whole-atmosphere-monthly-mean_ch4_$(date -v-2m "+%B_%Y" | tr '[:upper:]' '[:lower:]').txt.zip
+    if ! output=$(curl -q https://www.gosat.nies.go.jp/en/recent-global-ch4.html > rg-ch4.html); then
+	echo "$output" >$2
+	return $?
+    fi
+    data_link=$(egrep -o 'href="\.\./assets/[^"]+"' rg-ch4.html | head -1 | sed 's|href="\.\./\(.*\)"|\1|g')
+    set -- https://www.gosat.nies.go.jp/$data_link
     local wamm_zip="$(basename $1)"
     if ! output=$(curl --connect-timeout 5 -q $1 > $wamm_zip); then
         echo "$output" >&2
@@ -32,7 +43,7 @@ gosatghg ()
     	 { fmt = "Latest %s-%02d monthly CH4 mean value: %s (ppb) | trend value: %s (ppb)\n" }
          { printf( fmt, $1, $2, $3, $4 ) }
     '
-    rm whole-atmosphere*.*
+    rm rg-ch4.html $wamm_zip $wamm_text
     return
 }
 gosatghg 2> /tmp/.latest.er
